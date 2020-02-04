ca49e505dea2f691418e4f61fcc09e1365f89f3a
tribaeka <egor924462@gmail.com>
2020-02-04 14:16:03 +0300

    diploma-server: add dictionary and search builds

--- src/main/java/by/gsu/pms/controller/UtilController.java
+++ src/main/java/by/gsu/pms/controller/UtilController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -47,6 +49,26 @@ public class UtilController {
     @GetMapping("experiences")
     public List<Experience> getExperiences() {
         return experienceRepo.findAll();
+    }
+
+    @GetMapping("search-builds")
+    public Map<String, String> getSearchBuilds() {
+        return new HashMap<String, String>() {{
+            put("#Angular Front-end Developer", "JavaScript Git Angular HTML CSS SASS");
+            put("#React Front-end Developer", "JavaScript Git React HTML CSS SASS");
+            put("#Java Back-end Developer", "Java JavaEE Spring Git XML");
+            put("JavaScript Back-end Developer", "NodeJS JavaScript Git Express");
+            put("Python Developer", "Python Git Machine Learning");
+            put("Data Analytic", "Data Analytics MySQL Python");
+            put("DevOps", "Docker");
+            put("for test #Angular Front-end Developer", "JavaScript Git Angular HTML CSS SASS");
+            put("for test #React Front-end Developer", "JavaScript Git React HTML CSS SASS");
+            put("for test #Java Back-end Developer", "Java JavaEE Spring Git XML");
+            put("for test JavaScript Back-end Developer", "NodeJS JavaScript Git Express");
+            put("for test Python Developer", "Python Git Machine Learning");
+            put("for test Data Analytic", "Data Analytics MySQL Python");
+            put("for test DevOps", "Docker");
+        }};
     }
 
 }
